33c1cd111458111827191f52c70549ea48b4ce4d
ulianaboiko <157517980+ulianaboiko@users.noreply.github.com>
2025-01-09 22:42:02 +0200

    separated js for lesson 1 and 2

---
+++ src/js/lesson-1.js
@@ -0,0 +1,81 @@
+"use strict";
+
+// let userName = "Uliana"; //string- все що влапках (рядок)  текст
+// let counter = NaN; //тип number
+// let isLogin = true; //boolean - only yes or no
+// let age; //undefine
+// const userChoice = null; //nul
+// const bigNumber = 100090903093n; //bigint
+// const key = Symbol("mykey"); //symbol
+// alert("Hello");
+
+// const isUserStaysSite = confirm("Залишаєтеся на сайті?");
+// console.log(isUserStaysSite);
+
+// const userAge = prompt("Вкажітьсвій вік");
+// console.log(parseInt(userAge) + 10);
+
+// Завдання 1
+// const str = "Привіт";
+// const num = 123;
+// const flag = true;
+// const txt = "true";
+
+// console.log(str, txt, num, flag);
+// console.log("в змінній str тип данних:", typeof str);
+// console.log("в змінній num тип данних:", typeof num);
+// console.log("в змінній flag тип данних:", typeof flag);
+// console.log("в змінній txt тип данних:", typeof txt);
+
+// Завдання 2
+// const nameUser = "Uliana";
+// const isFlagBlue = false;
+// const cart = null;
+// const bigNumer = 7776889n;
+// const key = Symbol("smt important");
+
+// let nthg;
+// let numer = 456;
+// let bookName = "Yellowface";
+// let isBlack = true;
+// let sandQuantity = 356892n;
+
+// Завдання 3
+// як я робила:
+// const myAge = prompt("Вкажіть ваш вік");
+// const friendAge = prompt("Вкажіть вік друга");
+// console.log(parseInt(myAge) + parseInt(friendAge));
+// як на уроці це завдання виконали:
+// const myAge = 16;
+// const myFriendsAge = 15;
+// const sumAge = myAge + myFriendsAge;
+// console.log(sumAge);
+
+// Завдання 4
+// const a = 467;
+// const b = 4994;
+// const c = a + b;
+// console.log(c);
+
+// Завдання 5
+// const group = "FE_39_23_Students_G1";
+// console.log("Я навчаюся в GoITeens в групі:", group);
+
+// завдання 6
+// const yourName = prompt("Яке ваше ім'я?");
+// console.log(yourName);
+
+// Творчі завдання:
+// 1
+// const nam = "MacBook Pro 13";
+// let price = 100000;
+// console.log("Обрано:", nam, "ціна за штуку:", price, "кредитів");
+// price = 20000;
+// console.log("Обрано:", nam, "ціна за штуку:", price, "кредитів");
+
+// 2
+// const product = "iPhone 15";
+// let quantity = 5;
+// console.log("На складі є", product, "у кількості", quantity, "штук.");
+// quantity = 10;
+// console.log("На складі є", product, "у кількості", quantity, "штук.");
--- src/js/lesson-2.js
+++ src/js/lesson-2.js
@@ -1,85 +1,5 @@
 "use strict";
 
-// let userName = "Uliana"; //string- все що влапках (рядок)  текст
-// let counter = NaN; //тип number
-// let isLogin = true; //boolean - only yes or no
-// let age; //undefine
-// const userChoice = null; //nul
-// const bigNumber = 100090903093n; //bigint
-// const key = Symbol("mykey"); //symbol
-// alert("Hello");
-
-// const isUserStaysSite = confirm("Залишаєтеся на сайті?");
-// console.log(isUserStaysSite);
-
-// const userAge = prompt("Вкажітьсвій вік");
-// console.log(parseInt(userAge) + 10);
-
-// Завдання 1
-// const str = "Привіт";
-// const num = 123;
-// const flag = true;
-// const txt = "true";
-
-// console.log(str, txt, num, flag);
-// console.log("в змінній str тип данних:", typeof str);
-// console.log("в змінній num тип данних:", typeof num);
-// console.log("в змінній flag тип данних:", typeof flag);
-// console.log("в змінній txt тип данних:", typeof txt);
-
-// Завдання 2
-// const nameUser = "Uliana";
-// const isFlagBlue = false;
-// const cart = null;
-// const bigNumer = 7776889n;
-// const key = Symbol("smt important");
-
-// let nthg;
-// let numer = 456;
-// let bookName = "Yellowface";
-// let isBlack = true;
-// let sandQuantity = 356892n;
-
-// Завдання 3
-// як я робила:
-// const myAge = prompt("Вкажіть ваш вік");
-// const friendAge = prompt("Вкажіть вік друга");
-// console.log(parseInt(myAge) + parseInt(friendAge));
-// як на уроці це завдання виконали:
-// const myAge = 16;
-// const myFriendsAge = 15;
-// const sumAge = myAge + myFriendsAge;
-// console.log(sumAge);
-
-// Завдання 4
-// const a = 467;
-// const b = 4994;
-// const c = a + b;
-// console.log(c);
-
-// Завдання 5
-// const group = "FE_39_23_Students_G1";
-// console.log("Я навчаюся в GoITeens в групі:", group);
-
-// завдання 6
-// const yourName = prompt("Яке ваше ім'я?");
-// console.log(yourName);
-
-// Творчі завдання:
-// 1
-// const nam = "MacBook Pro 13";
-// let price = 100000;
-// console.log("Обрано:", nam, "ціна за штуку:", price, "кредитів");
-// price = 20000;
-// console.log("Обрано:", nam, "ціна за штуку:", price, "кредитів");
-
-// 2
-// const product = "iPhone 15";
-// let quantity = 5;
-// console.log("На складі є", product, "у кількості", quantity, "штук.");
-// quantity = 10;
-// console.log("На складі є", product, "у кількості", quantity, "штук.");
-
 // ЗАНЯТТЯ 2 Математичні оператори
 // console.log(3 ** 2); //піднесення до степеня
 // console.log(3 + 2); //Додавання
@@ -167,17 +87,17 @@
 // alert(sum);
 
 //Завдання 2.1
-const k1 = Math.round(parseInt("5px"));
-const k2 = Math.round(parseInt("12djd334"));
-const k3 = Math.round(parseInt("12.25asdwe12"));
-const k4 = Math.round(parseInt("qwqweeewq"));
-console.log(k1);
-console.log(k2);
-console.log(k3);
-console.log(k4);
+// const k1 = Math.round(parseInt("5px"));
+// const k2 = Math.round(parseInt("12djd334"));
+// const k3 = Math.round(parseInt("12.25asdwe12"));
+// const k4 = Math.round(parseInt("qwqweeewq"));
+// console.log(k1);
+// console.log(k2);
+// console.log(k3);
+// console.log(k4);
 
 // Завдання 3.3
-const height = 10;
-const diametr = 4;
-const volume = Math.PI * height * diametr;
-console.log("Обєм циліндра", volume.toFixed(2), "метрів кубічних");
+// const height = 10;
+// const diametr = 4;
+// const volume = Math.PI * height * diametr;
+// console.log("Обєм циліндра", volume.toFixed(2), "метрів кубічних");
