3e69732033957de0634920ddf2a72e2c720a6aac
ulianaboiko <157517980+ulianaboiko@users.noreply.github.com>
2025-01-20 23:57:35 +0200

    practice les-3

--- src/js/lesson-3.js
+++ src/js/lesson-3.js
@@ -42,84 +42,85 @@
 
 //Практичні завдання
 //1
-const myFullName = "Uliana Boiko";
-console.log(myFullName.length);
+// const myFullName = "Uliana Boiko";
+// console.log(myFullName.length);
 
-//2
-const userName = "uliana";
-console.log(userName.toUpperCase());
+// //2
+// const userName = "uliana";
+// console.log(userName.toUpperCase());
 
-//3
-const nName = "Uliana";
-console.log(nName.charAt(0)); //
+// //3
+// const nName = "Uliana";
+// console.log(nName.charAt(0)); //
 
-//4
-const mName = "Uliana Boiko";
-console.log(mName.indexOf(" "));
+// //4
+// const mName = "Uliana Boiko";
+// console.log(mName.indexOf(" "));
 
-//5
-const justName = "Uliana";
-const replaceLetterInName = justName.replace("U", "@");
-console.log(replaceLetterInName);
+// //5
+// const justName = "Uliana";
+// const replaceLetterInName = justName.replace("U", "@");
+// console.log(replaceLetterInName);
 
-//6
-const strtext = "JavaScript";
-console.log(strtext.substring(0, 4));
+// //6
+// const strtext = "JavaScript";
+// console.log(strtext.substring(0, 4));
 
-//2.1
-const customerName = "Уляна";
-const customerSurname = "Бойко";
-const time = "17.00";
-const roomNumber = "26";
-const message = `Гість ${customerName} ${customerSurname} посиляється в ${time} номер ${roomNumber}`;
-console.log(message);
+// //2.1
+// const customerName = "Уляна";
+// const customerSurname = "Бойко";
+// const time = "17.00";
+// const roomNumber = "26";
+// const message = `Гість ${customerName} ${customerSurname} посиляється в ${time} номер ${roomNumber}`;
+// console.log(message);
 
-//2.2
-const notTrimmedSentense = "      Hello World!    ";
-const trimmedSentense = notTrimmedSentense.trim();
-console.log(trimmedSentense);
+// //2.2
+// const notTrimmedSentense = "      Hello World!    ";
+// const trimmedSentense = notTrimmedSentense.trim();
+// console.log(trimmedSentense);
 
 //2.3
-const textString = "Ваш баланс поповнено на 100000";
+// const textString = "Ваш баланс поповнено на 100000";
 // const textStringWithAddedSymbols = textString.padEnd(1, "0");
 // console.log(textStringWithAddedSymbols);
-console.log(textString.length);
+// console.log(textString.length);
 
-//2.4
-const orderMsg = `Ви замовляєте 15 тістечок.`;
-console.log(orderMsg.includes(15));
-console.log(orderMsg.indexOf("1"));
+// //2.4
+// const orderMsg = `Ви замовляєте 15 тістечок.`;
+// console.log(orderMsg.includes(15));
+// console.log(orderMsg.indexOf("1"));
 
-//3.1
-const blacklistedWord1 = "спам";
-const blacklistedWord2 = "розпродаж";
+// //3.1
+// const blacklistedWord1 = "спам";
+// const blacklistedWord2 = "розпродаж";
 
-const string1 = "Привіт, я Ілон Маск, це не спам, пропоную тобі мільйон!";
-const string2 = "Найбільший РОЗПРОДАЖ цього тижня, не пропустіть!";
-const string3 = "Рекламна кампанія #jsIsAwesome";
-console.log(
-  `Рядок ${string1} включає слово ${blacklistedWord1} - ${string1
-    .toLowerCase()
-    .includes(blacklistedWord1)}`
-);
-console.log(
-  `Рядок ${string2} включає слово ${blacklistedWord2} - ${string2
-    .toLowerCase()
-    .includes(blacklistedWord2)}`
-);
-if (
-  string1.toLowerCase().includes(blacklistedWord1) ||
-  string1.toLowerCase().includes(blacklistedWord2)
-) {
-  console.log("Повідомлення містить рекламу або спам");
-} else {
-  console.log("Повідомлення містить небажаний контент");
-}
+// const string1 = "Привіт, я Ілон Маск, це не спам, пропоную тобі мільйон!";
+// const string2 = "Найбільший РОЗПРОДАЖ цього тижня, не пропустіть!";
+// const string3 = "Рекламна кампанія #jsIsAwesome";
+// console.log(
+//   `Рядок ${string1} включає слово ${blacklistedWord1} - ${string1
+//     .toLowerCase()
+//     .includes(blacklistedWord1)}`
+// );
+// console.log(
+//   `Рядок ${string2} включає слово ${blacklistedWord2} - ${string2
+//     .toLowerCase()
+//     .includes(blacklistedWord2)}`
+// );
+// if (
+//   string1.toLowerCase().includes(blacklistedWord1) ||
+//   string1.toLowerCase().includes(blacklistedWord2)
+// ) {
+//   console.log("Повідомлення містить рекламу або спам");
+// } else {
+//   console.log("Повідомлення містить небажаний контент");
+// }
 
-//3.2
-const numberHours = отримане число / 60;
-const days = Math.floor(numberHours / 24);
-const hours = Math.floor(numberHours % 60);
-const modHours = String(hours).padStart(2, 0);
-const minutes = отримане число % 60;
-const modMinutes = String(minutes).padStart(2, 0);
+// //3.2
+// const askMinutes = prompt("Введіть якусь кількість хвилин:");
+// const numberHours = parseInt(askMinutes) / 60;
+// const hours = Math.floor(numberHours % 60);
+// const modHours = String(hours).padStart(2, 0);
+// const minutes = parseInt(askMinutes) % 60;
+// const modMinutes = String(minutes).padStart(2, 0);
+// alert(`${modHours}:${modMinutes}`);
