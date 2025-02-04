b1064d2ad3daa57ac3b72cb036d47a5e8719aebe
ulianaboiko <157517980+ulianaboiko@users.noreply.github.com>
2025-02-04 22:31:59 +0200

    lesson 6

--- src/js/lesson-5.js
+++ src/js/lesson-5.js
@@ -57,114 +57,114 @@
 
 //Практичні завдання
 //1
-let textContent;
-const browser = "Safari";
-switch (browser) {
-  case "Edge":
-    textContent = "You've got the Edge!";
-    break;
-  case "Chrome":
-  case "Firefox":
-  case "Safari":
-  case "Opera":
-    textContent = "Ми підтримуємо і ці браузери";
-    break;
-  default:
-    textContent = "Маємо надію, що ця сторінка виглядає добре!";
-}
-console.log(textContent);
+// let textContent;
+// const browser = "Safari";
+// switch (browser) {
+//   case "Edge":
+//     textContent = "You've got the Edge!";
+//     break;
+//   case "Chrome":
+//   case "Firefox":
+//   case "Safari":
+//   case "Opera":
+//     textContent = "Ми підтримуємо і ці браузери";
+//     break;
+//   default:
+//     textContent = "Маємо надію, що ця сторінка виглядає добре!";
+// }
+// console.log(textContent);
 
-//2
-const a = Number(prompt("Введіть ціле число від 0 до 3 :"));
-let result;
-switch (a) {
-  case 0:
-    result = 0;
-    break;
-  case 1:
-    result = 1;
-    break;
-  case 2:
-  case 3:
-    result = "2 , 3";
-    break;
-  default:
-    result = "Ви ввели невірне значення цілого числа";
-}
-console.log(result);
+// //2
+// const a = Number(prompt("Введіть ціле число від 0 до 3 :"));
+// let result;
+// switch (a) {
+//   case 0:
+//     result = 0;
+//     break;
+//   case 1:
+//     result = 1;
+//     break;
+//   case 2:
+//   case 3:
+//     result = "2 , 3";
+//     break;
+//   default:
+//     result = "Ви ввели невірне значення цілого числа";
+// }
+// console.log(result);
 
-//2.1
-const askNumb = Number(prompt("Введіть ціле число"));
-if (askNumb >= 55 && askNumb <= 99) {
-  alert("Число потрапляє в діапазон");
-} else {
-  alert("Число не потрапляє в діапазон");
-}
+// //2.1
+// const askNumb = Number(prompt("Введіть ціле число"));
+// if (askNumb >= 55 && askNumb <= 99) {
+//   alert("Число потрапляє в діапазон");
+// } else {
+//   alert("Число не потрапляє в діапазон");
+// }
 
-//2.2
-const age = 55;
-if (age >= 0 && age <= 16) {
-  console.log("Ви відноситесь до вікої групи  діти");
-} else if (age >= 17 && age <= 60) {
-  console.log("Ви відноситесь до вікої групи дорослі");
-} else if (age >= 61 && age <= 100) {
-  console.log("Ви відноситесь до вікої групи пенсіонери");
-}
+// //2.2
+// const age = 55;
+// if (age >= 0 && age <= 16) {
+//   console.log("Ви відноситесь до вікої групи  діти");
+// } else if (age >= 17 && age <= 60) {
+//   console.log("Ви відноситесь до вікої групи дорослі");
+// } else if (age >= 61 && age <= 100) {
+//   console.log("Ви відноситесь до вікої групи пенсіонери");
+// }
 
-//2.3
-const userName = prompt("Введіть ваше ім'я");
-const userSurename = prompt("Введіть ваше прізвище");
-if (userName.length > 4 && userSurename.length > 5) {
-  const fullNameLength = userName.length + userSurename.length;
-  alert(fullNameLength);
-} else alert("УПС");
+// //2.3
+// const userName = prompt("Введіть ваше ім'я");
+// const userSurename = prompt("Введіть ваше прізвище");
+// if (userName.length > 4 && userSurename.length > 5) {
+//   const fullNameLength = userName.length + userSurename.length;
+//   alert(fullNameLength);
+// } else alert("УПС");
 
-//2.4
-const num = Math.floor(Math.random() * 5 + 1);
-if (num === 1) {
-  console.log("1");
-} else if (num === 2) {
-  console.log("2");
-} else if (num === 3) {
-  console.log("3");
-} else if (num === 4) {
-  console.log("4");
-} else if (num === 5) {
-  console.log("5");
-}
-switch (num) {
-  case 1:
-    console.log("1");
-    break;
-  case 2:
-    console.log("2");
-    break;
-  case 3:
-    console.log("3");
-    break;
-  case 4:
-    console.log("4");
-    break;
-  case 5:
-    console.log("5");
-    break;
-}
+// //2.4
+// const num = Math.floor(Math.random() * 5 + 1);
+// if (num === 1) {
+//   console.log("1");
+// } else if (num === 2) {
+//   console.log("2");
+// } else if (num === 3) {
+//   console.log("3");
+// } else if (num === 4) {
+//   console.log("4");
+// } else if (num === 5) {
+//   console.log("5");
+// }
+// switch (num) {
+//   case 1:
+//     console.log("1");
+//     break;
+//   case 2:
+//     console.log("2");
+//     break;
+//   case 3:
+//     console.log("3");
+//     break;
+//   case 4:
+//     console.log("4");
+//     break;
+//   case 5:
+//     console.log("5");
+//     break;
+// }
 
-//2.5
-const lagn = "ua";
-let month;
-switch (lagn) {
-  case "ua":
-    console.log("Січень");
-    break;
-  case "en":
-    console.log("January");
-    break;
-  case "it":
-    console.log("Gennaio");
-    break;
-  case "fr":
-    console.log("Jenvier");
-    break;
-  default:
-}
+// //2.5
+// const lagn = "ua";
+// let month;
+// switch (lagn) {
+//   case "ua":
+//     console.log("Січень");
+//     break;
+//   case "en":
+//     console.log("January");
+//     break;
+//   case "it":
+//     console.log("Gennaio");
+//     break;
+//   case "fr":
+//     console.log("Jenvier");
+//     break;
+//   default:
+// }
---
+++ src/js/lesson-6.js
@@ -0,0 +1,138 @@
+"use strict";
+
+// Цикли
+
+// let counter = 0;
+// while (counter < 10) {
+//   counter += 1;
+//   console.log(counter);
+// }
+
+// const PASSWORD = "11122122"; //якщо назва зміееої капсом то вона важлива
+// let userPassword;
+// ("11122122");
+// while (userPassword !== PASSWORD) {
+//   //вивдтть
+//   userPassword = prompt("Введіть пароль:");
+// }
+
+// do {
+//   userPassword = prompt("Введіть пароль:");
+// } while (userPassword !== PASSWORD);
+
+// while (userPassword !== PASSWORD) {
+//   userPassword = propmt("hhhh");
+// }
+
+// Цикл for
+// for (let i = 0;  i < 10; i++){
+// console.log(i)
+// }
+
+// const text = "JavaWScript";
+
+// for (let i = 0; i < text.length; i++) {
+//   if (i === 4) {
+//     continue;
+//   }
+//   console.log(text[i]);
+// }
+
+//Практичні завдання
+//1
+// let number = 1;
+// while (number <= 5) {
+//   console.log(number);
+//   number = number + 1;
+// }
+
+// do {
+//   console.log(number);
+//   number = number + 1;
+// } while (number <= 5);
+
+// for (let i = 1; i <= 5; i++) {
+//   console.log(i);
+// }
+
+// //3
+// for (let number = 0; number < 5; number++) {
+//   console.log(`Число ${number}`);
+// }
+// //на while
+// let num = 0;
+// while (num < 5) {
+//   console.log(`Число ${num}`);
+//   num += 1;
+// }
+
+//4
+// for (let i = 1; i < 1000; i += 2) {
+//   console.log(i);
+// }
+// for (let a = 1; a < 1000; a++) {
+//   if (a % 2 === 0) {
+//     continue;
+//   } else {
+//     console.log(a);
+//   }
+//   console.log(i);
+// }
+// for (let b = 0; i < 1000; b++) {
+//   if (b % 2 !== o) {
+//     console.log(b);
+//   }
+// }
+
+// //2.1
+// let num = 0;
+// do {
+//   num = Number(prompt("Введіть число більше 100: "));
+// } while (num < 100);
+
+//2.2
+// const employees = 70;
+// let totalSalary = 0;
+// for (let i = 1; i <= employees; i++) {
+//   const salary = Math.round(Math.random() * (5000 - 500) + 500);
+//   totalSalary += salary;
+// }
+// console.log(totalSalary);
+
+//2.3
+// const min = 5;
+// const max = 10;
+// let sumOfDoubleNumbers = 0;
+
+// for (let i = min; i <= max; i++) {
+//   if (i % 2 === 0) {
+//     sumOfDoubleNumbers += i;
+//   }
+// }
+// console.log(sumOfDoubleNumbers);
+
+// //2.4
+// let userResponse;
+// do {
+//   userResponse = prompt("Введіть число більше 100");
+//   if (userResponse === null) {
+//     alert("Скасовано");
+//     break;
+//   }
+//   userResponse = Number(userResponse);
+// } while (userResponse <= 100 || isNaN(userResponse));
+
+// //2.5
+// const min = 0;
+// const max = 5;
+// let sumOfOddNumber = 0;
+// for (let i = min; i <= max; i++) {
+//   if (i % 2 !== 0) {
+//     sumOfOddNumber += i;
+//   }
+// }
+// console.log(sumOfOddNumber);
+
+//додаткове
+const askNumber = prompt("Введіть число:");
+let totalSum;
